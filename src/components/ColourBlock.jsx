b01b125756d8afa0215e92628b7ba4348c2c851f
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-23 21:01:05 +1000

    feat: colour generator useable no save

---
+++ src/components/ColourBlock.jsx
@@ -0,0 +1,11 @@
+
+export default function ColourBlock({colourEntry}) {
+
+    return (
+        <div className="colourBlock" style={{backgroundColor: colourEntry.hex}}>
+            <h1>{colourEntry.hex}</h1>
+            <h2>{colourEntry.themeName + "-" + colourEntry.strength}</h2>
+
+        </div>
+    )
+}
---
+++ src/functions/themeGenerator.js
@@ -0,0 +1,28 @@
+import { colorblender, extend } from "colorblender";
+import nameExtension from "colorblender/extensions/name";
+import { paletten } from "paletten";
+
+extend([nameExtension])
+
+
+export function generateTones(baseColour, themeName = ""){
+    let newThemeName = themeName.toLocaleLowerCase().replaceAll(" ", "-") || colorblender(baseColour).name().toLocaleLowerCase().replaceAll(" ", "-")
+    let rawPalettenOutput = paletten(baseColour)
+    let finalisedOutput = {
+        name: newThemeName,
+        colours: []
+    };
+
+    let formattedColoursList = Object.keys(rawPalettenOutput).map((key) => {
+        return {
+            strength: key,
+            hex: rawPalettenOutput[key],
+            rgb: colorblender(rawPalettenOutput[key]).rgb(),
+            themeName: newThemeName
+        }
+    });
+
+    finalisedOutput.colours = formattedColoursList
+
+    return finalisedOutput
+}
